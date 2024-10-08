3c87f969919a4ae9744b5c8cce258da3b12ceb42
Bibiano06 <bibianocarros@gmail.com>
2024-10-08 20:35:44 -0300

    Atualizado

--- app/src/main/java/com/example/projetofirebase/FormHome.kt
+++ app/src/main/java/com/example/projetofirebase/FormHome.kt
@@ -3,9 +3,9 @@ package com.example.projetofirebase
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class MainActivity : AppCompatActivity() {
+class FormHome : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_form_home)
     }
 }
