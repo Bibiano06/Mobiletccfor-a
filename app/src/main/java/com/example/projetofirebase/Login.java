4fab952057be6e51ddb3d02e6573e8b1d4ccb274
Junior <claudianosilvajunior373@gmail.com>
2024-10-13 21:08:01 -0300

    new

---
+++ app/src/main/java/com/example/projetofirebase/Login.java
@@ -0,0 +1,28 @@
+package com.example.projetofirebase;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class Login extends AppCompatActivity {
+
+    Button btnCadastro;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        btnCadastro = findViewById(R.id.btnTela_cadastro);
+
+        btnCadastro.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Login.this, FormCadastro.class);
+                startActivity(intent);
+            }
+        });
+    }
+}
