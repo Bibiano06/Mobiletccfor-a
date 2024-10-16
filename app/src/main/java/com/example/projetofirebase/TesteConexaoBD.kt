bd07069438123969c417337326980b78d8439259
Bibiano06 <bibianocarros@gmail.com>
2024-10-16 12:31:37 -0300

    atualizado

---
+++ app/src/main/java/com/example/projetofirebase/TesteConexaoBD.kt
@@ -0,0 +1,11 @@
+package com.example.projetofirebase
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class TesteConexaoBD : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_teste_conexao_bd)
+    }
+}
--- app/src/main/java/com/example/projetofirebase/conexao.java
+++ app/src/main/java/com/example/projetofirebase/conexao.java
@@ -4,9 +4,26 @@ import android.os.StrictMode;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.SQLException;
 
 public class conexao {
 
+    public static Connection conectar (){
+     Connection conn = null;
+     try {
+         StrictMode.ThreadPolicy politica;
+         politica = new StrictMode.ThreadPolicy.Builder().permitAll().build();
+         StrictMode.setThreadPolicy(politica);
+         Class.forName("net.sourceforge.jtds.jdbc.Driver");
+         conn = DriverManager.getConnection("jdbc:jtds:sqlserver://192.168.56.1"+
+                 "databaseName=bd_centrotreinamento;user=sa;password=@ITB123456;");
 
+     } catch (SQLException e) {
+         e.getMessage();
+     } catch (ClassNotFoundException e){
+         e.printStackTrace();
+     }
+     return conn;
+
+    }
 }
-
