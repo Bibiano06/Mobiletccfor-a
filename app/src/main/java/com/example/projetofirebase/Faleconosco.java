6bca200daa1f22b8722d33507914c55a0335f976
Bibiano06 <bibianocarros@gmail.com>
2024-10-16 15:48:59 -0300

    atualizado

---
+++ app/src/main/java/com/example/projetofirebase/Faleconosco.java
@@ -0,0 +1,15 @@
+package com.example.projetofirebase;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class Faleconosco extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_faleconosco);
+
+    }
+}
