60a59c486bb2d64c0c41dca90def94190551bec2
Bibiano06 <bibianocarros@gmail.com>
2024-10-22 09:11:46 -0300

    atualizado

--- app/src/main/java/com/example/projetofirebase/Modalidades.java
+++ app/src/main/java/com/example/projetofirebase/Modalidades.java
@@ -1,6 +1,9 @@
 package com.example.projetofirebase;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -10,6 +13,20 @@ public class Modalidades extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_modalidades);
+
+        Button abrindo_tela = findViewById(R.id.btn_home);
+
+
+        abrindo_tela.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            Intent intent = new Intent(Modalidades.this, FormHome.class);
+
+            startActivity(intent);
+
+            }
+        });
 
     }
 
